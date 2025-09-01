c6664a93f3bb285aee435fa96adf6d0f49f0760d
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-01 13:08:35 +0330

    products

--- script.js
+++ script.js
@@ -4,37 +4,37 @@ let products = {
       productName: "Black Scarf",
       category: "Scarf",
       price: "30",
-      image: "scarf.webp",
+      image: "img/scarf.webp",
     },
     {
       productName: "Red Scarf",
       category: "Scarf",
       price: "30",
-      image: "scarf2.webp",
+      image: "img/scarf2.webp",
     },
     {
       productName: "Black T-Shirt",
       category: "T-Shirt",
       price: "49",
-      image: "TShirt.webp",
+      image: "img/TShirt.webp",
     },
     {
       productName: "Blue T-Shirt",
       category: "T-Shirt",
       price: "79",
-      image: "TShirt2.jpg",
+      image: "img/TShirt2.jpg",
     },
     {
       productName: "Black Pants",
       category: "Pants",
       price: "99",
-      image: "Pants.webp",
+      image: "img/Pants.webp",
     },
     {
       productName: "Black (Type2) Pants",
       category: "Pants",
       price: "109",
-      image: "Pants2.webp",
+      image: "img/Pants2.webp",
     },
   ],
 };
@@ -42,12 +42,15 @@ let products = {
 for (let i of products.data) {
   let card = document.createElement("div");
   card.classList.add("card", i.category);
+
   let imgContainer = document.createElement("div");
   imgContainer.classList.add("image-container");
+
   let image = document.createElement("img");
   image.setAttribute("src", i.image);
+
   imgContainer.appendChild(image);
   card.appendChild(imgContainer);
 
-  document.getElementById("products");
+  document.getElementById("products").appendChild(card);
 }
