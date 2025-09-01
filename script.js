803bfacd81f9059a498e86c8e7cb0e1a6645db47
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-01 12:23:26 +0330

    js part1

---
+++ script.js
@@ -0,0 +1,44 @@
+let products = {
+  data: [
+    {
+      productName: "Black Scarf",
+      category: "Scarf",
+      price: "30",
+      image: "scarf.webp",
+    },
+     {
+      productName: "Red Scarf",
+      category: "Scarf",
+      price: "30",
+      image: "scarf2.webp",
+    },
+     {
+      productName: "Black T-Shirt",
+      category: "T-Shirt",
+      price: "49",
+      image: "TShirt.webp",
+    },
+     {
+      productName: "Blue T-Shirt",
+      category: "T-Shirt",
+      price: "79",
+      image: "TShirt2.jpg",
+    },
+     {
+      productName: "Black Pants",
+      category: "Pants",
+      price: "99",
+      image: "Pants.webp",
+    },
+       {
+      productName: "Black (Type2) Pants",
+      category: "Pants",
+      price: "109",
+      image: "Pants2.webp",
+    }
+  ],
+};
+
+for(let i of products.data){
+    
+}
