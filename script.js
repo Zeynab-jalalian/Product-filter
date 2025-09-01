77b17ecff53a7ac35210a1a749b6477b3330c220
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-01 13:51:18 +0330

    finish

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ let products = {
 
 for (let i of products.data) {
   let card = document.createElement("div");
-  card.classList.add("card", i.category);
+  card.classList.add("card", i.category, "hide");
 
   let imgContainer = document.createElement("div");
   imgContainer.classList.add("image-container");
@@ -60,11 +60,54 @@ for (let i of products.data) {
   name.innerText = i.productName.toUpperCase();
   container.appendChild(name);
 
-  let price=document.createElement("h6");
-  price.innerText="$"+i.price;
+  let price = document.createElement("h6");
+  price.innerText = "$" + i.price;
   container.appendChild(price);
 
   card.appendChild(container);
 
   document.getElementById("products").appendChild(card);
 }
+
+function filterProduct(value) {
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  let elements=document.querySelectorAll(".card");
+  elements.forEach(element=>{
+    if(value == "All"){
+        element.classList.remove("hide");
+    }else{
+        if(element.classList.contains(value)){
+            element.classList.remove("hide");
+        }
+        else{
+            element.classList.add("hide");
+        }
+    }
+  })
+}
+
+document.getElementById("search").addEventListener("click",()=>{
+    let searchInput=document.getElementById("search-input").value;
+    let product_name=document.querySelectorAll(".product-name");
+    let cards=document.querySelectorAll(".card");
+
+    product_name.forEach((pn,index)=>{
+        if(pn.innerText.includes(searchInput.toUpperCase())){
+             cards[index].classList.remove("hide");
+        }else{
+            cards[index].classList.add("hide");
+        }
+    })
+})
+
+window.onload = () => {
+  filterProduct("All");
+};
